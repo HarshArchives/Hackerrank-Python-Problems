05351d18fadd1fd940daf3ed699b0a7832bf93e5
geekyharsh05 <hp424805@gmail.com>
2023-05-02 22:14:02 +0530

    :sparkles: feat: add Python Solution

---
+++ hackerranksolution/If_else.py
@@ -0,0 +1,34 @@
+"""
+@author geekyharsh05
+
+Python If-Else
+
+Task 
+Given an integer, n, perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Input Format
+A single line containing a positive integer, n.
+
+Constraints
+1 <= n <= 100
+
+Output Format
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+The Solution is given below
+"""
+
+n = int(input())  # taking user input
+if n % 2 != 0:    
+    print("Weird")
+else:
+    if n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
---
+++ hackerranksolution/SayHelloToPython.py
@@ -0,0 +1,17 @@
+"""
+@author geekyharsh05
+
+Input Format:
+You do not need to read any input in this challenge.
+
+Output Format:
+Print Hello, World! to stdout.
+
+Sample Output:
+
+Hello, World!
+
+The Solution is given below
+"""
+
+print("Hello, World!")
